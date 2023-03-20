e2b1dd36422ba783b5e79f70e6358936aada0824
Christian Alexandre <53078353+alechristian@users.noreply.github.com>
2023-03-20 18:31:25 -0300

    adição de testes

--- app_busca/test/widget_test.dart
+++ app_busca/test/widget_test.dart
@@ -5,26 +5,42 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:app_busca/app/pages/home/home_controller.dart';
+import 'package:app_busca/app/pages/home/home_page.dart';
+import 'package:app_busca/utils/enums/model_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:app_busca/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('HomePage should render', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const AppWidget());
+    await tester.pumpWidget(const MaterialApp(home: HomePage()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the AppBar is present
+    expect(find.byType(AppBar), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify that text widgtes are present
+    expect(find.text("Faça aqui"), findsOneWidget);
+    expect(find.text("Escreva aqui."), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    //Verify that the TextFiel is present
+    expect(find.byType(TextField), findsOneWidget);
+
+    //Verify that the TextButton is present
+    expect(find.byType(TextButton), findsOneWidget);
+  });
+
+  test('Teste setValidaCampo', () {
+    final controller = HomeController();
+    //!testando condicao desabilitado
+    controller.buscar.text = '12345';
+    controller.setValidaCampo('');
+    expect(controller.validadeButton, equals(stateButton.desabled));
+    //!testando condicao habilitado
+    controller.buscar.text = '123456';
+    controller.setValidaCampo('');
+    expect(controller.validadeButton, equals(stateButton.enabladed));
   });
 }
